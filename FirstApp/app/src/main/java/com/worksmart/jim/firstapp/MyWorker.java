5aed76427aed9f1e2ef82dc0edfa83d259ad133e
JaymK <jkathaka@gmail.com>
2018-10-23 18:19:36 +0300

    Added MyWorker Java Class

---
+++ FirstApp/app/src/main/java/com/worksmart/jim/firstapp/MyWorker.java
@@ -0,0 +1,11 @@
+package com.worksmart.jim.firstapp;
+
+/*
+** Created by Jimmy K
+ */
+
+public class MyWorker {
+    public static int doubleTheValue (int value) {
+        return value * 2;
+    }
+}
